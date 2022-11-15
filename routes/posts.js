e6a62fd3a89947c58cb81946b8d192c56d3fdf4c
Sameer Saxena <59393128+Sampai28@users.noreply.github.com>
2022-11-15 11:17:06 +0530

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -13,21 +13,7 @@ router.post("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
-//update a post
 
-router.put("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (post.userId === req.body.userId) {
-      await post.updateOne({ $set: req.body });
-      res.status(200).json("the post has been updated");
-    } else {
-      res.status(403).json("you can update only your post");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 //delete a post
 
 router.delete("/:id", async (req, res) => {
@@ -37,23 +23,24 @@ router.delete("/:id", async (req, res) => {
       await post.deleteOne();
       res.status(200).json("the post has been deleted");
     } else {
-      res.status(403).json("you can delete only your post");
+      res.status(403).json("Delete only your post please!");
     }
   } catch (err) {
     res.status(500).json(err);
   }
 });
-//like / dislike a post
+
+//like / unlike a post
 
 router.put("/:id/like", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (!post.likes.includes(req.body.userId)) {
       await post.updateOne({ $push: { likes: req.body.userId } });
-      res.status(200).json("The post has been liked");
+      res.status(200).json("Liked!");
     } else {
       await post.updateOne({ $pull: { likes: req.body.userId } });
-      res.status(200).json("The post has been disliked");
+      res.status(200).json("Unliked!");
     }
   } catch (err) {
     res.status(500).json(err);
@@ -70,7 +57,7 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-//get timeline posts
+//get all posts
 
 router.get("/timeline/all", async (req, res) => {
   try {
